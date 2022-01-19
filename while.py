d5a1a478456fa9e1c92d436c0f9d4119df5cee2d
ionpostolache <ion_postolache00@yahoo.com>
2022-01-19 21:44:02 +0200

    initial

---
+++ while.py
@@ -0,0 +1,34 @@
+'''
+WHILE
+se evalueaza o singura conditie
+atat timp cat conditia este adevarata, se executa codul aferent acesteia
+ex:
+atat timp cat inca ploua:
+    'tin umbrela deasupra capului'
+'''
+
+benzina_ramasa = 7
+while benzina_ramasa > 0:
+    # accelerez
+    print('Vruum Vruum!')
+    # in timp ce accelerez se consuma un l de benzina
+    benzina_ramasa = benzina_ramasa - 1
+    # se pot intercala alte notiuni invatate
+    # daca mai avem doar 1 l, atentionam soferul
+    if benzina_ramasa == 1: # == este operator de comparare
+        print('Atentie! Mai ai doar 1 l de benzina')
+
+print('Stop! Ai ramas fara benzina')
+
+'''
+exercitiu:
+super mario are 3 vieti
+atat timp cat mai are vieti, te poti juca (print start game)
+incepe jocul si mario pierde cate o viata
+optional poti printa ('Ai pierdut o viata. Mai ai x vieti')
+la final printeaza 'game over'
+'''
+
+
+
+
